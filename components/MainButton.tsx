ff0f87427676a201bc1b1a3e770bb76716d1d6cd
MJ <alswns8495@naver.com>
2025-01-24 10:19:43 +0900

    ✨ feat: 메인 버튼 구현, 테일윈드 uninstall

--- components/MainButton.tsx
+++ components/MainButton.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import styles from "./MainButton.module.css";
+import styles from "./MainButton.module.scss";
 
 interface MainButtonProps {
   text: string;
