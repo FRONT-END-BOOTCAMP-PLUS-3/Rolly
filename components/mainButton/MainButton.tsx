f0fdc6ae67537f3fd66ee37edb13395c38c4eea7
jisooyoon <yoonmallang@gmail.com>
2025-02-04 14:31:35 +0900

    ✨ feat: 메인버튼 diabled 속성 추가

--- components/mainButton/MainButton.tsx
+++ components/mainButton/MainButton.tsx
@@ -3,12 +3,18 @@ import styles from "./MainButton.module.scss";
 
 interface MainButtonProps {
   text: string;
-  onClick?: () => void;
+  onClick: () => void;
+  disabled: boolean;
 }
 
-const MainButton: React.FC<MainButtonProps> = ({ text, onClick }) => {
+const MainButton: React.FC<MainButtonProps> = ({ text, onClick, disabled }) => {
   return (
-    <button className={styles["main-button"]} onClick={onClick}>
+    <button
+      type="button"
+      className={styles["main-button"]}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {text}
     </button>
   );
