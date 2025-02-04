f2daf6727b755dd529c9e2aa7d0b6d88e5b5b0bd
MJ <alswns8495@naver.com>
2025-02-04 12:08:07 +0900

    ✨ feat: login 페이지 임시 modal 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import AuthInput from "@/components/authInput/AuthInput";
 import MainButton from "@/components/mainButton/MainButton";
 import BackButton from "@/components/backButton/BackButton";
+import Modal from "@/components/modal/Modal";
 import styles from "./page.module.scss";
 import supabase from "@/utils/supabase/supabaseClient";
 
@@ -13,31 +14,37 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [isModalOpen, setModalOpen] = useState(false);
 
   const handleLogin = async () => {
     if (!email || !password) {
       setErrorMessage("이메일과 비밀번호를 입력해주세요.");
+      setModalOpen(true);
       return;
     }
 
     try {
-      // Supabase 로그인 로직
       const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
         password: password,
       });
 
-      if (error) throw error; // 오류 발생 시 에러를 throw
+      if (error) throw error;
 
-      console.log("로그인 성공:", data); // 로그인 성공시 사용자 정보를 콘솔에 출력
-      router.push("/intro"); // 로그인 성공 후 디렉션
+      console.log("로그인 성공:", data);
+      router.push("/intro");
     } catch (error) {
       console.error("로그인 중 오류 발생:", error);
-      setErrorMessage(
-        (error as Error).message || "로그인 중 오류가 발생했습니다."
-      );
+      if (error instanceof Error) {
+        setErrorMessage(error.message || "로그인 중 오류가 발생했습니다.");
+      } else {
+        setErrorMessage("로그인 중 오류가 발생했습니다.");
+      }
+      setModalOpen(true);
     }
   };
+
+  const closeModal = () => setModalOpen(false);
 
   return (
     <div className={styles["login-container"]}>
@@ -56,15 +63,25 @@ const Login = () => {
 
         <AuthInput
           label="비밀번호"
-          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능해요"
+          placeholder="8자 이상이며, 숫자와 특수문자를 포함해야 합니다."
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        {errorMessage && (
-          <p className={styles["error-message"]}>{errorMessage}</p>
-        )}
+        <Modal
+          contents={[
+            {
+              title: "로그인 오류",
+              body: errorMessage,
+            },
+          ]}
+          onConfirm={closeModal}
+          onCancel={closeModal}
+          isOpen={isModalOpen}
+          confirmText="확인"
+          cancelText="닫기"
+        />
       </div>
 
       <div className={styles["login-footer"]}>
