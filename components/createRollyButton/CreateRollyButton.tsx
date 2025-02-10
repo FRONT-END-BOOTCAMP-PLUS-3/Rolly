63c7a277f00d0cdc11f1bbed098e09b64d58b8ab
jisooyoon <yoonmallang@gmail.com>
2025-02-10 14:27:59 +0900

    ✨ feat: CreateRollyButton 컴포넌트에 className props 추가하여 스타일 확장 가능하도록 수정

--- components/createRollyButton/CreateRollyButton.tsx
+++ components/createRollyButton/CreateRollyButton.tsx
@@ -3,11 +3,19 @@ import styles from "./CreateRollyButton.module.scss";
 
 interface CreateRollyButtonProps {
   onClick?: () => void;
+  className?: string;
 }
 
-const CreateRollyButton: React.FC<CreateRollyButtonProps> = ({ onClick }) => {
+const CreateRollyButton: React.FC<CreateRollyButtonProps> = ({
+  onClick,
+  className,
+}) => {
   return (
-    <button className={styles["create-rolly-button"]} onClick={onClick}>
+    <button
+      type="button"
+      className={`${styles["create-rolly-button"]} ${className}`}
+      onClick={onClick}
+    >
       <img src="/icons/create-rolly.svg" alt="롤리 만들기!" />
       <p>롤리 만들기!</p>
     </button>
