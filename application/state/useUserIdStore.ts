15210130821fc330e62be22b8db184889b4fae4d
jisooyoon <yoonmallang@gmail.com>
2025-02-10 16:15:40 +0900

    ✨ feat: 세션스토리지에 값 저장하여 새로고침 시에도 상태 유지되도록 store 수정

--- application/state/useUserIdStore.ts
+++ application/state/useUserIdStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { UUID } from "@/types/common";
 
 type State = {
@@ -9,13 +10,17 @@ type Action = {
   setUserId: (type: State["userId"]) => void;
 };
 
-const useUserIdStore = create<State & Action>((set) => ({
-  userId:
-    localStorage.getItem("userId") || "00000000-0000-0000-0000-000000000000",
-  setUserId: (userId) => {
-    localStorage.setItem("userId", userId); // id를 localStorage에 저장
-    set({ userId });
-  },
-}));
+const useUserIdStore = create(
+  persist<State & Action>(
+    (set) => ({
+      userId: "00000000-0000-0000-0000-000000000000",
+      setUserId: (userId) => set({ userId }),
+    }),
+    {
+      name: "userId",
+      storage: createJSONStorage<State & Action>(() => sessionStorage),
+    }
+  )
+);
 
 export default useUserIdStore;
