902d458a662d83e36bc2d30bc9f1ed188d55d75c
jisooyoon <yoonmallang@gmail.com>
2025-02-10 16:23:29 +0900

    ✨ feat: 세션스토리지에 값 저장하여 새로고침 시에도 상태 유지되도록 store 수정

--- application/state/useRollyCreateStore.ts
+++ application/state/useRollyCreateStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type State = {
   type: number;
@@ -10,11 +11,19 @@ type Action = {
   setTitle: (title: State["title"]) => void;
 };
 
-const useRollyCreateStore = create<State & Action>((set) => ({
-  type: 0,
-  title: "",
-  setType: (type) => set({ type: type }),
-  setTitle: (title) => set({ title: title }),
-}));
+const useRollyCreateStore = create(
+  persist<State & Action>(
+    (set) => ({
+      type: 0,
+      title: "",
+      setType: (type) => set({ type: type }),
+      setTitle: (title) => set({ title: title }),
+    }),
+    {
+      name: "rollyCreateData",
+      storage: createJSONStorage<State & Action>(() => sessionStorage),
+    }
+  )
+);
 
 export default useRollyCreateStore;
