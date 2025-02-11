d5615b56bbd037ac4e7271bd1df1684d074ccbce
MJ <alswns8495@naver.com>
2025-02-11 16:07:34 +0900

    ✨ feat: (anon)rollies/[id] 페이지 추가,헤더 뒤로가기 버튼 홈 버튼으로 변경

--- components/homeButton/HomeButton.tsx
+++ components/homeButton/HomeButton.tsx
@@ -1,23 +1,27 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import styles from "./HomeButton.module.scss";
 
 interface HomeButtonProps {
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 const HomeButton: React.FC<HomeButtonProps> = ({ onClick }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    router.push("/pages");
+  };
+
   return (
-    <button onClick={onClick} className={styles["home-button"]}>
-      <img
-        src="/icons/home.svg" //
-        alt="home button"
-      />
+    <button onClick={handleClick} className={styles["home-button"]}>
+      <img src="/icons/home.svg" alt="home button" />
     </button>
   );
 };
 
 export default HomeButton;
-
-//비로그인 시 : 시작화면으로 이동 로직추가 필요
-//로그인 시 : 메인 화면으로 이동  로직추가 필요
