4b34f356a02def308f7652292bc200b589735d33
gkdud7351 <gkdud7351@gmail.com>
2025-02-12 22:26:38 +0900

    🐛 fix: 만든 롤리함에서 롤리 만들기 버튼 클릭 시 모달 띄우기

--- app/member/rollies/created/page.tsx
+++ app/member/rollies/created/page.tsx
@@ -5,14 +5,19 @@ import CreateRollyButton from "@/components/createRollyButton/CreateRollyButton"
 import Header from "@/components/header/Header";
 import RollyItem from "@/components/rollyItem/RollyItem";
 import styles from "./page.module.scss";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { CreatedRollyDto } from "@/application/usecases/rolly/dto/CreatedRollyDto";
 import useUserStore from "@/application/state/useUserStore";
+import useToggle from "@/hooks/useToggle";
+import Modal from "@/components/modal/Modal";
+import useRollyCreateStore from "@/application/state/useRollyCreateStore";
+import { InputFormData } from "@/components/modal/Modal.type";
 
 const CreatedRollies: React.FC = () => {
   const [rollyItems, setRollyItems] = useState<CreatedRollyDto[]>([]);
+  const [isCreateRollyModalOpen, toggleCreateRollyModal] = useToggle(false);
+  const { setType, setTitle } = useRollyCreateStore();
   const { userId } = useUserStore();
   const router = useRouter();
 
@@ -25,6 +30,27 @@ const CreatedRollies: React.FC = () => {
     };
     fetchCreatedRollies();
   }, [userId]);
+
+  const handleConfirm = (inputFormData?: InputFormData) => {
+    if (
+      inputFormData &&
+      inputFormData.modal_radio &&
+      inputFormData.modal_text
+    ) {
+      switch (inputFormData.modal_radio) {
+        case "개인용":
+          setType(1);
+          break;
+        case "단체용":
+          setType(2);
+          break;
+        default:
+          break;
+      }
+      setTitle(inputFormData.modal_text);
+    }
+    router.push("/member/rollies/create");
+  };
 
   const handleItemClick = (id: number) => {
     router.push(`/member/rollies/${id}`);
@@ -53,9 +79,10 @@ const CreatedRollies: React.FC = () => {
         <Header leftContent={<BackButton />} title="내가 만든 롤리" />
       </div>
       <div className={styles["create-rolly-button"]}>
-        <Link href={"/member/rollies/create/"}>
-          <CreateRollyButton />
-        </Link>
+        <CreateRollyButton
+          onClick={toggleCreateRollyModal}
+          className={styles["create-rolly-btn"]}
+        />
       </div>
       <p className={styles["list-title"]}>롤리 리스트</p>
       <div className={styles["list"]}>
@@ -73,6 +100,25 @@ const CreatedRollies: React.FC = () => {
           />
         ))}
       </div>
+      <Modal
+        contents={[
+          {
+            title: "롤리의 타입을 선택해주세요",
+            body: "개인용은 받는 사람이 답장할 수 있어요!",
+            input: "radio",
+            radioOptions: ["개인용", "단체용"],
+          },
+          {
+            title: "롤리의 이름을 작성해주세요",
+            body: "최대 12자까지 가능해요!",
+            input: "text",
+            maxLength: 12,
+          },
+        ]}
+        onConfirm={handleConfirm}
+        onCancel={toggleCreateRollyModal}
+        isOpen={isCreateRollyModalOpen}
+      />
     </div>
   );
 };
