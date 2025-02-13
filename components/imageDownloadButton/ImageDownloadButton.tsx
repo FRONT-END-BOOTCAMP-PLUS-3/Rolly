958b01c1d90c93d6abc38d7297e6f91529ef60b3
MJ <alswns8495@naver.com>
2025-02-13 12:26:56 +0900

    🐛  fix: 사진 저장 사진 잘려서 나오는버그 픽스

--- components/imageDownloadButton/ImageDownloadButton.tsx
+++ components/imageDownloadButton/ImageDownloadButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import html2canvas from "html2canvas";
+import { toPng } from "html-to-image";
 import Image from "next/image";
 
 interface ImageDownloadButtonProps {
@@ -13,24 +13,31 @@ const ImageDownloadButton: React.FC<ImageDownloadButtonProps> = ({
 }) => {
   const handleDownload = async () => {
     if (targetRef.current) {
-      const canvas = await html2canvas(targetRef.current);
-      const image = canvas.toDataURL("image/png");
-      const link = document.createElement("a");
-      link.href = image;
-      link.download = "downloaded-image.png"; // 다운로드할 파일명
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+      try {
+        const dataUrl = await toPng(targetRef.current, {
+          cacheBust: true, // 필요에 따라 캐시를 방지합니다.
+          // 추가 옵션들은 필요에 따라 설정할 수 있습니다.
+        });
+
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "downloaded-image.png"; // 다운로드할 파일명
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      } catch (error) {
+        console.error("Oops, something went wrong!", error);
+      }
     }
   };
 
   return (
-    <button type="button" className={`$ ${className}`} onClick={handleDownload}>
+    <button type="button" className={className} onClick={handleDownload}>
       <Image
         src="/icons/image-download.svg"
         width={24}
         height={24}
-        alt="사진 다운로드"
+        alt="Download Image"
       />
     </button>
   );
